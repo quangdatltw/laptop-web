6ea114556f004380258b6ffef0c3399b8e0c2fd8
Raki <124845426+quangdatltw@users.noreply.github.com>
2025-06-22 23:42:27 +0700

    Bean test
    Bean is instance of java class that used in the project, controlled by Spring container. When run project, all class become bean and controlled to work together

--- src/main/java/com/laptop/web/WebApplication.java
+++ src/main/java/com/laptop/web/WebApplication.java
@@ -2,12 +2,17 @@ package com.laptop.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 
 @SpringBootApplication
 public class WebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebApplication.class, args);
+        ApplicationContext web = SpringApplication.run(WebApplication.class, args);
+        for (String beanName : web.getBeanDefinitionNames()) {
+            System.out.println("Bean: " + beanName);
+        }
     }
 
 }
