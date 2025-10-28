aa95c5acb3c13311bfb2efa28450acf07e2fa31e
Raki <124845426+quangdatltw@users.noreply.github.com>
2025-10-28 21:18:57 +0700

    Init MVC structure

---
+++ src/main/java/com/laptop/web/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.laptop.web.controller;
+
+import com.laptop.web.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("")
+    public String getHomePage() {
+        return userService.handleHello();
+    }
+}
